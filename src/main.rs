dea0d00839c44374143f1163daaaeee1e3707a74
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-06-29 17:53:56 -0400

    Got the example working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 extern crate actix;
 extern crate actix_web;
+extern crate env_logger;
+
+#[macro_use]
+extern crate log;
 
 use actix::prelude::*;
-use actix_web::{App, HttpRequest, server};
+use actix_web::{http, middleware, server, App, HttpRequest};
+use std::{thread, time};
 
 #[derive(Message)]
 struct BackgroundTask;
@@ -11,46 +16,49 @@ struct BackgroundTask;
 struct BackgroundTaskActor;
 
 impl Actor for BackgroundTaskActor {
-    type Context = Context<Self>;
-}
+    type Context = SyncContext<Self>;
 
-impl actix::Supervised for BackgroundTaskActor {}
-
-impl SystemService for BackgroundTaskActor {
-    fn service_started(&mut self, _: &mut Context<Self>) {
-        println!("Background task actor started up")
+    fn started(&mut self, _: &mut SyncContext<Self>) {
+        info!("Background task actor started up")
     }
 }
 
 impl Handler<BackgroundTask> for BackgroundTaskActor {
     type Result = ();
 
-    fn handle(&mut self, _: BackgroundTask, _: &mut Context<Self>) {
-        println!("Ran background task")
+    fn handle(&mut self, _: BackgroundTask, _: &mut SyncContext<Self>) {
+        info!("Starting background task");
+        thread::sleep(time::Duration::new(4,0));
+        info!("Finished background task");
     }
 }
 
-fn index(_req: HttpRequest) -> &'static str {
-    let background_task = System::current().registry().get::<BackgroundTaskActor>();
-    background_task.do_send(BackgroundTask {});
+fn index(req: HttpRequest<AppState>) -> &'static str {
+    req.state().background_actor.do_send(BackgroundTask {});
 
-    "Minimal response"
+    "Background task started\n"
+}
+
+struct AppState {
+    background_actor: Addr<Syn, BackgroundTaskActor>,
 }
 
 fn main() {
+    ::std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
     let sys = actix::System::new("background-worker-example");
+    let bt_actor = SyncArbiter::start(1, move || BackgroundTaskActor::default());
 
-    BackgroundTaskActor.start();
-
-    let srv = server::new(|| App::new().resource("/", |r| r.f(index)))
+    server::new(move ||
+            App::with_state(AppState { background_actor: bt_actor.clone() })
+                .middleware(middleware::Logger::default())
+                .resource("/", |r| r.method(http::Method::GET).with(index))
+        )
         .bind("127.0.0.1:8000")
-        .unwrap();
+        .unwrap()
+        .start();
 
-    // Normally I had this chained off the previous call, pulled it off to
-    // isolate this exception:
-    //
-    //  thread 'main' panicked at 'System is not running'
-    srv.start();
-
+    info!("Starting up server on 127.0.0.1:8000");
     sys.run();
 }
